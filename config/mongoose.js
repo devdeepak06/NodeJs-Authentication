90b1b3f10961a4a5f10525673d620a4fe40d17e2
devdeepak06 <k.deepak2217@gmail.com>
2023-09-07 18:03:47 +0530

    update db

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,7 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-const db = 'mongodb+srv://kdeepak:oIS9cjMj9Ex3cdZ5@nodejsauth.lccqj6d.mongodb.net/'
-console.log("MongoDB URI:", db);
-mongoose
-  .connect(db)
-  .then(() => {
-    console.log("database connected successfully");
-  })
-  .catch((error) => {
-    console.error("not connected to the database", error);
-  });
+     const mongoose = require("mongoose");
+     require("dotenv").config()
+
+
+     mongoose.connect(process.env.db)
+         .then(() => { console.log('database conected successfully') })
+         .catch((error) => { console.log('not connected database') })
