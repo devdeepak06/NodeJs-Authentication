c2f230d19c30192ae86e39bb7567c9339654969f
devdeepak06 <k.deepak2217@gmail.com>
2023-09-07 18:28:36 +0530

    update db

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,17 @@
-     const mongoose = require("mongoose");
-     require("dotenv").config()
+const mongoose = require("mongoose");
+require("dotenv").config();
+const db =
+  "mongodb+srv://kdeepak:oIS9cjMj9Ex3cdZ5@nodejsauth.lccqj6d.mongodb.net/";
+console.log("MongoDB URI:", db);
 
-
-     mongoose.connect(process.env.db)
-         .then(() => { console.log('database conected successfully') })
-         .catch((error) => { console.log('not connected database') })
+// dotenv.config();
+// console.log(process.env.db);
+mongoose
+  //   .connect(process.env.db)
+  .connect(db)
+  .then(() => {
+    console.log("database conected successfully");
+  })
+  .catch((error) => {
+    console.log("not connected database");
+  });
